2757502d7fb131b9922a445a9fd21889d8755059
Shinn <295037128@qq.com>
2019-01-25 14:26:26 +0800

    优化 dashboard line chart

--- src/containers/home/Comments.jsx
+++ src/containers/home/Comments.jsx
@@ -5,39 +5,103 @@ import styled from 'styled-components';
 import { Card, List, Avatar } from 'antd';
 import ErrorBoundary from '../../components/feedback/ErrorBoundary';
 
-const data = [
-  {
-    title: 'Ant Design Title 1',
-  },
-  {
-    title: 'Ant Design Title 2',
-  },
-  {
-    title: 'Ant Design Title 3',
-  },
-  {
-    title: 'Ant Design Title 4',
-  },
-  {
-    title: 'Ant Design Title 5',
-  },
-];
+const StyledContent = styled.div`
+  span {
+    font-size: 12px;
+  }
+  
+  span.avatar {
+    margin-right: 5px;
+  }
+`;
+
+const Details = styled.div`
+  line-height: 22px;
+  padding: 5px 0;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box; //作为弹性伸缩盒子模型显示。
+  -webkit-box-orient: vertical; //设置伸缩盒子的子元素排列方式--从上到下垂直排列
+  -webkit-line-clamp: 2; //显示的行
+`;
+
 
 class Comments extends React.PureComponent {
+  
+  renderDescription = (item) => {
+    return (
+      <StyledContent>
+        <Details title={item.description}>{item.description}</Details>
+        <span className="avatar">{item.user}</span>
+        <span className="time">{item.create_at}</span>
+      </StyledContent>
+    );
+  };
   
   renderItem = () => (item) => {
     return (
       <List.Item>
         <List.Item.Meta
-          avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-          title={<a href="#">{item.title}</a>}
-          description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+          avatar={<Avatar src={item.avatar_url} />}
+          title={item.title}
+          description={this.renderDescription(item)}
         />
       </List.Item>
     );
   };
   
   render() {
+    const data = [
+      {
+        id: 1,
+        user: 'Shinn',
+        avatar_url: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+        title: '呵呵哒~~',
+        description: '上班错过公交，于是追着车跑，一个骑着电动车的人在公车旁边喊：“加油啊！ ”我心底突然涌上一股暖流，刚想感慨这世界的美好，那人又接着喊道：“师傅快加油，别让后面这逼追上了！',
+        create_at: '2019-01-23 22:54',
+      },
+      {
+        id: 2,
+        user: 'Shinn',
+        avatar_url: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+        title: '呵呵哒~~',
+        description: '一曲相思~~~',
+        create_at: '2019-01-23 22:54',
+      },
+      {
+        id: 3,
+        user: 'Shinn',
+        avatar_url: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+        title: '呵呵哒~~',
+        description: '悟空：人生最痛苦的事莫过于一阵风吹来过后，猪在呢，马在呢，人不在了！最最痛苦的莫过于还有一个二货这时会用最大的嗓门喊：大师兄！师傅被妖怪抓走了！以证明他的存在，然后接着发呆。',
+        create_at: '2019-01-23 22:54',
+      },
+      {
+        id: 4,
+        user: 'Shinn',
+        avatar_url: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+        title: '呵呵哒~~',
+        description: '一曲相思~~~',
+        create_at: '2019-01-23 22:54',
+      },
+      {
+        id: 5,
+        user: 'Shinn',
+        avatar_url: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+        title: '呵呵哒~~',
+        description: '一曲相思~~~',
+        create_at: '2019-01-23 22:54',
+      },
+      {
+        id: 6,
+        user: 'Shinn',
+        avatar_url: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+        title: '呵呵哒~~',
+        description: '一曲相思~~~',
+        create_at: '2019-01-23 22:54',
+      },
+    ];
+
     return (
       <Card title="用户评论">
         <List
--- src/containers/home/charts/LineChart.jsx
+++ src/containers/home/charts/LineChart.jsx
@@ -1,62 +1,98 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import { Chart, Geom, Axis, Tooltip, Legend } from "bizcharts";
+import { Chart, Geom, Axis, Tooltip, Legend } from 'bizcharts';
 import DataSet from '@antv/data-set';
 
 import ErrorBoundary from '../../../components/feedback/ErrorBoundary';
 
-class LineChart extends React.PureComponent {
-  render() {
-    const data = [
-      { month: "Jan", Tokyo: 7.0, London: 3.9 },
-      { month: "Feb", Tokyo: 6.9, London: 4.2 },
-      { month: "Mar", Tokyo: 9.5, London: 5.7 },
-      { month: "Apr", Tokyo: 14.5, London: 8.5 },
-      { month: "May", Tokyo: 18.4, London: 11.9 },
-      { month: "Jun", Tokyo: 21.5, London: 15.2 },
-      { month: "Jul", Tokyo: 25.2, London: 17.0 },
-      { month: "Aug", Tokyo: 26.5, London: 16.6 },
-      { month: "Sep", Tokyo: 23.3, London: 14.2 },
-      { month: "Oct", Tokyo: 18.3, London: 10.3 },
-      { month: "Nov", Tokyo: 13.9, London: 6.6 },
-      { month: "Dec", Tokyo: 9.6, London: 4.8 }
-    ];
+const list = [
+  { id: 1, pv: 320, download: 204, average_pv: 300, month: '一月' },
+  { id: 2, pv: 540, download: 642, average_pv: 423, month: '二月' },
+  { id: 3, pv: 654, download: 344, average_pv: 266, month: '三月' },
+  { id: 4, pv: 769, download: 345, average_pv: 643, month: '四月' },
+  { id: 5, pv: 907, download: 568, average_pv: 745, month: '五月' },
+  { id: 6, pv: 975, download: 423, average_pv: 765, month: '六月' },
+  { id: 7, pv: 2351, download: 654, average_pv: 867, month: '七月' },
+  { id: 8, pv: 634, download: 234, average_pv: 300, month: '八月' },
+  { id: 9, pv: 765, download: 674, average_pv: 546, month: '九月' },
+  { id: 10, pv: 984, download: 348, average_pv: 799, month: '十月' },
+  { id: 11, pv: 1244, download: 354, average_pv: 675, month: '十一月' },
+  { id: 12, pv: 1568, download: 543, average_pv: 987, month: '十二月' },
+];
 
+class LineChart extends React.PureComponent {
+  axisLine = {
+    stroke: '#dddddd',
+    lineWidth: 1,
+  };
+  
+  axisTickLine = {
+    lineWidth: 1,
+    stroke: '#ccc',
+  };
+  
+  crosshairs = {
+    type: 'y',
+  };
+  
+  cols = {
+    month: {
+      alias: '月份',
+      range: [0, 1],
+    },
+  };
+  
+  axisTitle = {
+    autoRotate: true, // 是否需要自动旋转，默认为 true
+    offset: -20, // 设置标题 title 距离坐标轴线的距离
+    textStyle: {
+      fontSize: '12',
+      textAlign: 'center',
+      fill: '#999',
+      fontWeight: 'bold',
+      rotate: '0',
+    }, // 坐标轴文本属性配置
+    position: 'end', // 标题的位置 'start' || 'center' || 'end'
+  };
+  
+  axisLabel = {
+    formatter: val => `${val}万`,
+  };
+  
+  render() {
     const ds = new DataSet();
-    const dv = ds.createView().source(data);
+    const dv = ds.createView().source(list);
+    
     dv.transform({
-      type: "fold",
-      fields: ["Tokyo", "London"],
-      // 展开字段集
-      key: "city",
-      // key字段
-      value: "temperature" // value字段
+      type: 'rename',
+      map: {
+        pv: '访问量',
+        download: '下载量',
+        average_pv: '平均访问量',
+      },
+    }).transform({
+      type: 'fold',
+      fields: ['访问量', '下载量', '平均访问量'], // 展开字段集
+      key: 'name', // key字段
+      value: 'pvc', // value字段
+    }).transform({
+      type: 'rename',
+      map: {
+        pvc: '统计数',
+      },
     });
 
-    const cols = {
-      month: {
-        range: [0, 1]
-      }
-    };
-
     return (
-      <Chart height={400} data={dv} scale={cols} forceFit>
+      <Chart forceFit height={400} data={dv} scale={this.cols} padding="auto">
         <Legend />
-        <Axis name="month" />
-        <Axis name="temperature" label={{ formatter: val => `${val}°C` }} />
-        <Tooltip crosshairs={{ type: "y" }} />
-        <Geom type="line" position="month*temperature" size={2} shape="smooth" color={"city"} />
-        <Geom
-          type="point"
-          position="month*temperature"
-          size={4}
-          shape="circle"
-          color={"city"}
-          style={{
-            stroke: "#fff",
-            lineWidth: 1
-          }}
-        />
+        
+        <Axis name="month" line={this.axisLine} />
+        <Axis title={this.axisTitle} name="统计数" line={this.axisLine} tickLine={this.axisTickLine} label={this.axisLabel} />
+        
+        <Tooltip crosshairs={this.crosshairs} />
+        
+        <Geom type="line" position="month*统计数" size={2} shape="smooth" color="name" />
+        <Geom type="point" position="month*统计数" color="name" style={{ stroke: '#fff', lineWidth: 1 }} />
       </Chart>
     );
   }
