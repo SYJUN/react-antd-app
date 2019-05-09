22b87fd41aacbc01d56104e4b0bbe89408a44fe3
shinn <yanjun.shi@linkedcare.cn>
2019-05-09 14:01:22 +0800

    添加兼容 windows 平台配置

--- scripts/build.js
+++ scripts/build.js
@@ -7,6 +7,10 @@ const { execSync } = require('child_process');
 
 class WebpackBuild {
   constructor() {
+    // 兼容 windows 和 mac 平台
+    const SEPARATOR = process.platform === 'win32' ? ';' : ':';
+    this.env = Object.assign({}, process.env);
+    this.env.PATH = path.resolve('./node_modules/.bin') + SEPARATOR + this.env.PATH;
     this.distPath = path.resolve('./dist');
 
     process.env.NODE_ENV = 'production';
@@ -18,12 +22,16 @@ class WebpackBuild {
   }
 
   removeDistFolder() {
-    console.log(colors.green(`Remove dist folder!\n`));
+    console.log(colors.green('Remove dist folder!\n'));
     fse.removeSync(this.distPath);
   }
 
   build() {
-    execSync('webpack --progress', { stdio: 'inherit' });
+    execSync('webpack --progress', { 
+      stdio: 'inherit', 
+      cwd: process.cwd(),
+      env: this.env,
+    });
   }
 }
 
--- scripts/start.js
+++ scripts/start.js
@@ -1,5 +1,6 @@
 /* eslint no-console: 0 */
 const { spawn } = require('child_process');
+// 跨平台兼容版第三方库 ： const spawn = require('cross-spawn');  Node.js v6 版本
 
 class WebpackStart {
   constructor() {
@@ -11,7 +12,10 @@ class WebpackStart {
   }
   
   start() {
-    spawn('webpack-dev-server', ['--progress', '--compress'], { stdio: 'inherit' });
+    spawn('webpack-dev-server', ['--progress', '--compress'], { 
+      stdio: 'inherit', 
+      shell: process.platform === 'win32',    // 仅在当前运行环境为 Windows 时，才使用 shell
+    }); 
   }
 }
 
